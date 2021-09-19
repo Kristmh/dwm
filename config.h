c091df6d9e1675271978220112ba65387d060668
kristian <haugerud.kristian@gmail.com>
2021-09-19 18:45:01 +0200

    add 2 shortcuts

--- config.h
+++ config.h
@@ -62,6 +62,8 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY,			                  XK_w,		   spawn,		       SHCMD("brave") },
+	{ MODKEY,			                  XK_s,		   spawn,		       SHCMD("flameshot gui") },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
